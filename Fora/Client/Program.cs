d75445440fa9d27641b4baa60c51b4504d556441
filipforsstrom <filipforsstrom@gmail.com>
2022-04-07 08:59:27 +0200

    MudBlazor init

--- Fora/Client/Program.cs
+++ Fora/Client/Program.cs
@@ -3,8 +3,10 @@ global using Microsoft.AspNetCore.Components.Authorization;
 using Fora.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
