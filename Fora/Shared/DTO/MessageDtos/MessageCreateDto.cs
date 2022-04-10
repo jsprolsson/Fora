21b885ab3df7f0245843bf48b3db0c22ee3c8d8d
jsprolsson <jesper.p.olsson@hotmail.com>
2022-04-10 20:36:32 +0200

    message gets passed through, no userid and no statechange

--- Fora/Shared/DTO/MessageDtos/MessageCreateDto.cs
+++ Fora/Shared/DTO/MessageDtos/MessageCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Fora.Shared.DTO.MessageDtos
     public class MessageCreateDto
     {
         //What is the message
+        [Required]
+        [StringLength(300, ErrorMessage ="Message can't exceed 300 characters.", MinimumLength = 1)]
         public string Message { get; set; } = String.Empty;
         //In what thread
         public int ThreadId { get; set; }
